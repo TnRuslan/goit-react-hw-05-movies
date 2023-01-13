20617adc8a38ef7f80193d100c16f7a540557cb9
Ruslan Tendora <ruslantendora@gmail.com>
2023-01-13 23:51:59 +0200

    add fetch movies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import { Loyout } from './Loyout/Loyout';
 import { Movies } from './pages/Movies';
+import { Home } from './pages/Home';
+import { MovieDetails } from './pages/MovieDetails';
 
 export const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Loyout />}>
-          <Route
-            path="home"
-            element={
-              <div>
-                <h1>Home</h1>
-              </div>
-            }
-          ></Route>
+          <Route path="home" element={<Home />}></Route>
           <Route path="movies" element={<Movies />}></Route>
+          <Route path="movies/:id" element={<MovieDetails />} />
         </Route>
       </Routes>
     </>
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+export const Home = () => {
+  const [trendingMovies, setTrendingMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      'https://api.themoviedb.org/3/trending/movie/day?api_key=3ff904f01b180e8aa08fc9b52d5b2a33'
+    )
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.results);
+        setTrendingMovies([...data.results]);
+      });
+  }, []);
+
+  return (
+    <ul>
+      {trendingMovies.map(movie => {
+        return (
+          <li key={movie.id}>
+            <Link to={`movies/${movie.id}`}>{movie.title}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
